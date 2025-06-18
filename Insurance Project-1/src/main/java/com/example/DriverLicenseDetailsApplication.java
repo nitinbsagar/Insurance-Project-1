ad311062bf671d7b83e516ebc13a8897106295c2
nitinbsagar@gmail.com <nitinbsagar@gmail.com>
2025-06-18 14:55:30 +0530

    Insurance Project-1 All Task Done

---
+++ Insurance Project-1/src/main/java/com/example/DriverLicenseDetailsApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.example.serviceimpl.DriverLicenseServiceImpl;
+
+@SpringBootApplication
+public class DriverLicenseDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriverLicenseDetailsApplication.class, args);
+
+		
+		
+	}
+
+}
---
+++ Insurance Project-1/src/main/java/com/example/serviceimpl/DriverIdNotFoundException.java
@@ -0,0 +1,16 @@
+package com.example.serviceimpl;
+
+public class DriverIdNotFoundException extends RuntimeException{
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DriverIdNotFoundException (String message) {
+		super(message);
+	}
+	
+
+}
